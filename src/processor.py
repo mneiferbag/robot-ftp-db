f1ecf67a6c9869b9519f9dedd67bfb58fb0b04af
Markus Neifer <mneiferbag@users.noreply.github.com>
2022-12-02 17:22:39 +0100

    Replace mysql with postgres

--- src/processor.py
+++ src/processor.py
@@ -7,26 +7,20 @@ import os
 from ftplib import FTP
 from time import sleep
 
-import pymysql.cursors
+import psycopg2
 
 dbHost = os.environ.get('DB_HOST')
 dbPort = int(os.environ.get('DB_PORT'))
 dbName = os.environ.get('DB_NAME')
 dbUser = os.environ.get('DB_USER')
-dbPassword = os.environ.get('MYSQL_ROOT_PASSWORD')
+dbPassword = os.environ.get('POSTGRES_PASSWORD')
 
 ftpHost = os.environ.get('PUBLICHOST')
 ftpPort = int(os.environ.get('FTP_PORT'))
 ftpUser = os.environ.get('FTP_USER_NAME')
 ftpPassword = os.environ.get('FTP_USER_PASS')
 
-connection = pymysql.connect(host=dbHost,
-                         port=dbPort,
-                         user=dbUser,
-                         password=dbPassword,
-                         database=dbName,
-                         charset='utf8mb4',
-                         cursorclass=pymysql.cursors.DictCursor)
+connection = psycopg2.connect("dbname=" + dbName + " user=" + dbUser)
 ftp = FTP()
 ftp.connect(ftpHost, ftpPort)
 ftp.login(user=ftpUser, passwd=ftpPassword)
